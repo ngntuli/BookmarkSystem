defea309befb454539538bbeb6f2acafdb8e724d
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-07 16:52:53 +0200

    bookmark entity

---
+++ src/com/ngntuli/bookmark/entities/Bookmark.java
@@ -0,0 +1,31 @@
+package com.ngntuli.bookmark.entities;
+
+public class Bookmark {
+	private long id;
+	private String title;
+	private String profileUrl;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+}
