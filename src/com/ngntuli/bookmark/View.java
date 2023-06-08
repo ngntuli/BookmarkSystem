8e498c22a31f5184efdd8c051f4215bf16245066
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-08 22:05:02 +0200

    print line

--- src/com/ngntuli/bookmark/View.java
+++ src/com/ngntuli/bookmark/View.java
@@ -7,16 +7,17 @@ import com.ngntuli.bookmark.entities.User;
 
 public class View {
 	public static void bookmark(User user, Bookmark[][] bookmarks) {
-		System.out.println("\n" + user.getEmail() + " is bookmarking");
+		System.out.println(user.getEmail() + " is bookmarking");
 		for (int i = 0; i < BookmarkData.USER_BOOKMARK_LIMIT; i++) {
 			int typeOffset = (int) (Math.random() * BookmarkData.BOOMARK_TYPES_COUNT);
 			int bookmakOffset = (int) (Math.random() * BookmarkData.BOOKMARK_COUNT_PER_TYPE);
 
 			Bookmark bookmark = bookmarks[typeOffset][bookmakOffset];
-			
+
 			BookmarkController.getInstance().saveUserBookmark(user, bookmark);
 
 			System.out.println(bookmark);
 		}
+		System.out.println();
 	}
 }
