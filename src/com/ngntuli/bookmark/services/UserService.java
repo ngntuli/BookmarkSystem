f7bb8a8ce34f7d3026efd5c2625303fbaef55062
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-08 16:56:21 +0200

    user service

---
+++ src/com/ngntuli/bookmark/services/UserService.java
@@ -0,0 +1,34 @@
+package com.ngntuli.bookmark.services;
+
+import com.ngntuli.bookmark.dao.UserDao;
+import com.ngntuli.bookmark.entities.User;
+
+public class UserService {
+	private static UserService instance = new UserService();
+	private static UserDao dao = new UserDao();
+
+	private UserService() {
+	}
+
+	public static UserService getInstance() {
+		return instance;
+	}
+
+	public User createUser(long id, String email, String password, String firstName, String lastName, int gender,
+			String userType) {
+		User user = new User();
+		user.setId(id);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setGender(gender);
+		user.setUserType(userType);
+
+		return user;
+	}
+
+	public User[] getUsers() {
+		return dao.getUsers();
+	}
+}
