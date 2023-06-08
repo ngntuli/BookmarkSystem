03d78cb3882bb91a4342b855190b22866918dcd1
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-08 22:41:15 +0200

    web link test class with junit5

---
+++ test/com/ngntuli/bookmark/entities/WebLinkTest.java
@@ -0,0 +1,14 @@
+package com.ngntuli.bookmark.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WebLinkTest {
+
+	@Test
+	void testIsKidFriendlyEligible() {
+		fail("Not yet implemented");
+	}
+
+}
