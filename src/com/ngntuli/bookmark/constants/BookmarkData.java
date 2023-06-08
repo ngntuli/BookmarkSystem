a37c33ac2fc62714619e0cfcc7dbcb89dd3eaa2e
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-08 21:54:34 +0200

    bookmark data constants

---
+++ src/com/ngntuli/bookmark/constants/BookmarkData.java
@@ -0,0 +1,11 @@
+package com.ngntuli.bookmark.constants;
+
+public class BookmarkData {
+	private BookmarkData() {
+	}
+
+	public static final int USER_BOOKMARK_LIMIT = 5;
+	public static final int BOOKMARK_COUNT_PER_TYPE = 5;
+	public static final int BOOMARK_TYPES_COUNT = 3;
+	public static final int TOTAL_USER_COUNT = 5;
+}
