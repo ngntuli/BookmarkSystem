b555a1cc178a447c4ab97b4df2f8445de4853a8a
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-08 21:51:46 +0200

    saveUserBookmark method and revoke add method to DataStore

--- src/com/ngntuli/bookmark/dao/BookmarkDao.java
+++ src/com/ngntuli/bookmark/dao/BookmarkDao.java
@@ -2,9 +2,15 @@ package com.ngntuli.bookmark.dao;
 
 import com.ngntuli.bookmark.DataStore;
 import com.ngntuli.bookmark.entities.Bookmark;
+import com.ngntuli.bookmark.entities.UserBookmark;
 
 public class BookmarkDao {
 	public Bookmark[][] getBookmarks() {
 		return DataStore.getBookmarks();
+	}
+
+	public void saveUserBookmark(UserBookmark userBookmark) {
+		DataStore.add(userBookmark);
+
 	}
 }
