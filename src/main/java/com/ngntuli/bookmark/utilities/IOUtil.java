679e0fbbaccfd79a60b7033267510c117f020d33
ngntuli <mr.ng.ntuli@gmail.com>
2023-07-11 16:34:55 +0200

    add read and write methods

--- src/main/java/com/ngntuli/bookmark/utilities/IOUtil.java
+++ src/main/java/com/ngntuli/bookmark/utilities/IOUtil.java
@@ -1,10 +1,14 @@
 package com.ngntuli.bookmark.utilities;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 
 public class IOUtil {
@@ -17,6 +21,36 @@ public class IOUtil {
 				data[count] = line;
 				count++;
 			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String read(InputStream in) {
+		StringBuilder text = new StringBuilder();
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				text.append(line).append("\n");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return text.toString();
+	}
+
+	public static void write(String webpage, long id) {
+		try (BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream("pages/" + String.valueOf(id) + ".html"), "UTF-8"))) {
+			writer.write(webpage);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
