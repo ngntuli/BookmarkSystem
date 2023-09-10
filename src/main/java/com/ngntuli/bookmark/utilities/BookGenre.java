75b6b40fed62f85be9d53cd8b70480aba1f7b23a
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-10 12:34:52 +0200

    change constants to enum class

--- src/main/java/com/ngntuli/bookmark/utilities/BookGenre.java
+++ src/main/java/com/ngntuli/bookmark/utilities/BookGenre.java
@@ -1,18 +1,18 @@
 package com.ngntuli.bookmark.utilities;
 
-public class BookGenre {
-	private BookGenre() {
+public enum BookGenre {
+	ART("Art"), BIOGRAPHY("Biography"), CHILDREN("Children"), FICTION("Fiction"), HISTORY("History"),
+	MYSTERY("Mystery"), PHILOSOPHY("Philosophy"), RELIGION("Religion"), ROMANCE("Romance"), SELF_HELP("Self help"),
+	TECHNICAL("Technical");
+
+	private String name;
+
+	private BookGenre(String name) {
+		this.name = name;
 	}
 
-	public static final String ART = "Art";
-	public static final String BIOGRAPHY = "Biography";
-	public static final String CHILDREN = "Children";
-	public static final String FICTION = "Fiction";
-	public static final String HISTORY = "History";
-	public static final String MYSTERY = "Mystery";
-	public static final String PHILOSOPHY = "Philosophy";
-	public static final String RELIGION = "Religion";
-	public static final String ROMANCE = "Romance";
-	public static final String SELF_HELP = "Self help";
-	public static final String TECHNICAL = "Technical";
+	public String getName() {
+		return name;
+	}
+
 }
